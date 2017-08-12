f64978b9d0cf68312fbc8e2fe39cb41517586743
Developer Davo <DeveloperDavo@users.noreply.github.com>
2017-08-12 15:26:25 +0200

    Remove countdown functionality
    because it was developed as a spike

--- src/application.js
+++ src/application.js
@@ -80,37 +80,6 @@ function Application() {
         $('#clock').css('background', 'linear-gradient(to top, ' + color + ' ' + percentage + '%, #333333 0%) bottom')
     }
 
-    function startCountdown(color) {
-        var countdown = setInterval(function () {
-            if (!isCountdownRunning) {
-                clearInterval(countdown);
-                return;
-            }
-            sessionSecondsLeft--;
-
-            if (sessionSecondsLeft === 0) {
-                // startBreakCountdown();
-                clearInterval(countdown);
-                $CLOCK_TIME.text(breakLengthInMinutes.toString().formatTime());
-                fillClock(color, 0);
-                return;
-            }
-
-            var sessionLengthInSeconds = sessionLengthInMinutes * 60;
-            var timeElapsedInSeconds = sessionLengthInSeconds - sessionSecondsLeft;
-            var percentageOfTimeElapsed = timeElapsedInSeconds / sessionLengthInSeconds * 100;
-
-            fillClock(color, percentageOfTimeElapsed);
-
-            $CLOCK_TIME.text(sessionSecondsLeft.toString().formatTime());
-        }, 1000);
-    }
-
-    function startBreakCountdown() {
-        $CLOCK_MODE.text(BREAK_MODE);
-        startCountdown("rgb(255, 68, 68)");
-    }
-
     function toggleCrementButtons(isDisabled) {
         var attr = 'disabled';
         $INCREMENT_SESSION.prop(attr, isDisabled);
@@ -123,7 +92,6 @@ function Application() {
         $START_PAUSE.click(function () {
             isCountdownRunning = !isCountdownRunning;
             if (isCountdownRunning) {
-                startCountdown(LIGHT_GREEN);
                 toggleCrementButtons(DISABLED);
                 $START_PAUSE.text(PAUSE);
             } else {
