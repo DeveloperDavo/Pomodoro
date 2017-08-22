2d80fc9b3c39b7eed1a42df59f2e5658ef2089bf
Developer Davo <DeveloperDavo@users.noreply.github.com>
2017-08-22 15:53:58 +0200

    Run babel when running grunt

--- GruntFile.js
+++ GruntFile.js
@@ -1,15 +1,28 @@
 module.exports = function (grunt) {
-  grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-      jasmine: {
-        src: ['src/**/*.js'],
-        options: {
-          specs: ['spec/**/*Spec.js'],
-          vendor: [
-              'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'
-          ]
+    require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jasmine: {
+            src: ['dist/**/*.js'],
+            options: {
+                specs: ['spec/**/*Spec.js'],
+                vendor: [
+                    'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'
+                ]
+            }
+        },
+        babel: {
+            options: {
+                sourceMap: true,
+                presets: ['env']
+            },
+            dist: {
+                files: {
+                    'dist/application.js': 'src/application.js'
+                }
+            }
         }
-      }
     });
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.registerTask("default", ["babel"]);
 };
